88722cb908383df3b3f6dc0d696b15a8e250b562
Luiscastrot <161341528+Luiscastrot@users.noreply.github.com>
2025-08-29 10:01:16 +0200

    F

--- Sales_Orders/Marco_data.py
+++ Sales_Orders/Marco_data.py
@@ -247,7 +247,9 @@ def main():
                   'currencyCode', 'lineItemcode', 'lineItemQty', 'lineItemUnitPrice', 'lineItemoption3',
                   'customFieldsorders_1001', 'lineItemDiscount', 'discountTotal', 'invoiceDate']
     
-    file_name = f"Sales_Orders_{start_date.strftime('%Y%m%d')}_{end_date.strftime('%Y%m%d')}.xlsx"
+    os.makedirs("tmp_files", exist_ok=True)
+    file_name = f"tmp_files/Sales_Orders_{start_date.strftime('%Y%m%d')}_{end_date.strftime('%Y%m%d')}.xlsx"
+
     
     all_sales_orders = []
 
